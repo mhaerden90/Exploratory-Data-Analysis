629790b9ee24e8648a8968da15e8fcdc3ab5a628
mhaerden90 <mhaerden@gmail.com>
2018-05-10 15:09:22 +0200

    fourth commit

--- plot1.R
+++ plot1.R
@@ -10,9 +10,6 @@ replace(data, data=="?", NA)
 subset <- c("1/2/2007", "2/2/2007")
 data <- data[data$Date %in% subset,]
 
-#Set to English
-Sys.setlocale("LC_TIME", "C")
-
 #Create plot#
 png(filename="plot1.png", width = 480, height = 480)
 hist(na.omit(as.numeric(as.character((data$Global_active_power)))), col="red", main="Global Active Power", xlab = "Global Active Power(kilowatts)", ylab="Frequency")
